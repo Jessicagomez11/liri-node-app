4d42887b7060d356f5605a23dfe7dfee1a77843f
Jessica  Gomez <jessicagomez@Jessicas-MacBook-Air.local>
2019-05-03 11:55:20 -0700

    The spotify api call function is working with user input

--- liri.js
+++ liri.js
@@ -1,5 +1,28 @@
-// var axios = require("axios")
 require("dotenv").config();
 
+var axios = require("axios")
 var keys = require("./keys.js");
-// var spotify = new Spotify(keys.spotify);
+var Spotify = require('node-spotify-api');
+var spotify = new Spotify(keys.spotify);
+
+
+var userAction = process.argv[2]
+var userSong = process.argv.slice(3).join('+')
+
+function spotifyCall(){
+ 
+spotify.search({ type: 'track', query: userSong }, function(err, data) {
+  if (err) {
+    return console.log('Error occurred: ' + err);
+  }
+ 
+console.log('_________________________________________________________')
+console.log("Artist: " + data.tracks.items[0].album.artists[0].name); 
+console.log("Album title: "+data.tracks.items[0].album.name); 
+console.log( data.tracks.items[0].album.artists[0].external_urls.spotify); 
+
+});
+}
+if (userAction === "spotify-this-song"){
+    spotifyCall()
+}

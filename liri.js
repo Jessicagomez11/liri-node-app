fd87fce23eca3d50a9c6575e4da6c7975ae8c0c6
Jessica  Gomez <jessicagomez@Jessicas-MacBook-Air.local>
2019-05-03 12:07:54 -0700

    added the song title information in the information that is returned to the user

--- liri.js
+++ liri.js
@@ -6,7 +6,7 @@ var Spotify = require('node-spotify-api');
 var spotify = new Spotify(keys.spotify);
 
 
-var userAction = process.argv[2]
+var userCommand = process.argv[2]
 var userSong = process.argv.slice(3).join('+')
 
 function spotifyCall(){
@@ -14,15 +14,23 @@ function spotifyCall(){
 spotify.search({ type: 'track', query: userSong }, function(err, data) {
   if (err) {
     return console.log('Error occurred: ' + err);
-  }
- 
-console.log('_________________________________________________________')
-console.log("Artist: " + data.tracks.items[0].album.artists[0].name); 
-console.log("Album title: "+data.tracks.items[0].album.name); 
-console.log( data.tracks.items[0].album.artists[0].external_urls.spotify); 
+} else {
+    console.log('__________________________________________________________________________________') 
+
+    console.log('Here are your results: ')
+    console.log("Song title: "+ userSong.split('+').join(' '))
+    console.log("Artist: " + data.tracks.items[0].album.artists[0].name); 
+    console.log("Album title: "+data.tracks.items[0].album.name); 
+    console.log("Listen on Spotify: "+data.tracks.items[0].album.artists[0].external_urls.spotify); 
+    console.log('__________________________________________________________________________________') 
+}
 
 });
+
 }
-if (userAction === "spotify-this-song"){
+
+
+
+if (userCommand === "spotify-this-song"){
     spotifyCall()
 }

ccc9c6df90b2bea4f8f9e5f38e0efa7075047978
Jessica  Gomez <jessicagomez@Jessicas-MacBook-Air.local>
2019-05-03 22:16:27 -0700

    The function for the omdb call is working

--- liri.js
+++ liri.js
@@ -1,5 +1,6 @@
 require("dotenv").config();
 
+var moment = require('moment');
 var axios = require("axios")
 var keys = require("./keys.js");
 var Spotify = require('node-spotify-api');
@@ -7,18 +8,18 @@ var spotify = new Spotify(keys.spotify);
 
 
 var userCommand = process.argv[2]
-var userSong = process.argv.slice(3).join('+')
+var userRequest = process.argv.slice(3).join('+')
 
 function spotifyCall(){
  
-spotify.search({ type: 'track', query: userSong }, function(err, data) {
+spotify.search({ type: 'track', query: userRequest }, function(err, data) {
   if (err) {
     return console.log('Error occurred: ' + err);
 } else {
     console.log('__________________________________________________________________________________') 
 
     console.log('Here are your results: ')
-    console.log("Song title: "+ userSong.split('+').join(' '))
+    console.log("Song title: "+ userRequest.split('+').join(' '))
     console.log("Artist: " + data.tracks.items[0].album.artists[0].name); 
     console.log("Album title: "+data.tracks.items[0].album.name); 
     console.log("Listen on Spotify: "+data.tracks.items[0].album.artists[0].external_urls.spotify); 
@@ -28,9 +29,40 @@ spotify.search({ type: 'track', query: userSong }, function(err, data) {
 });
 
 }
+//************************************************************************************************* 
 
+function bandsInTownCall(){ 
+ var changeCity = 0
+    axios.get('https://rest.bandsintown.com/artists/' +  userRequest + '/events?app_id=codingbootcamp')
+    .then(function (response) {
+        console.log('__________________________________________________________________________________') 
+
+        console.log(userRequest.split("+").join(' ') + " will be in "+ response.data[changeCity].venue.city +" on " + moment(response.data[changeCity].datetime).format('LLLL')+" at the "+ response.data[changeCity].venue.name+".")
+        console.log('__________________________________________________________________________________') 
+
+        // console.log(response.data);
+       
+
+
+    })
+    .catch(function (error) {
+        console.log(error);
+    });
+}
+
+//************************************************************************************************* 
 
 
 if (userCommand === "spotify-this-song"){
+
     spotifyCall()
+
+}else if (userCommand === "concert-this"){
+
+    bandsInTownCall()
+    
+}else if (userCommand === "movie-this"){
+
+}else if (userCommand === "do-what-it-says"){
+
 }
